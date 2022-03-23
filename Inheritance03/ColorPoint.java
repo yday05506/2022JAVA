cbf0a156b5283a98f8a058b8e247682930692089
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2022-03-23 11:52:14 +0900

    super()

--- Inheritance03/ColorPoint.java
+++ Inheritance03/ColorPoint.java
@@ -4,7 +4,8 @@ public class ColorPoint extends Point{
 	private String color;
 	
 	public ColorPoint(int x, int y, String color) {
-		super(x, y);
+		//super() 메소드는 1개만 기술
+		super(x, y);	//ColorPoint 생성자의 첫 번째 줄에 위치해야 함
 		this.color = color;
 	}
 	
