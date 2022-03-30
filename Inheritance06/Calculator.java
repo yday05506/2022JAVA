1c7fe07ff82d58227c38a4a28480b06c3b208d7d
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2022-03-30 12:15:15 +0900

    추상 클래스

---
+++ Inheritance06/Calculator.java
@@ -0,0 +1,7 @@
+package Inheritance06;
+
+abstract class Calculator {
+	public abstract int add(int a, int b);
+	public abstract int subtract(int a, int b);
+	public abstract double average(int[] a);
+}
