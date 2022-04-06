9ee8bf135d20ebba5f3da0ea93641325c5519814
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2022-04-06 12:18:31 +0900

    super()

--- TextInheritance05/EngHello.java
+++ TextInheritance05/EngHello.java
@@ -9,8 +9,8 @@ public class EngHello extends Greeting {
 	}
 	
 	public void test() {
-		System.out.print(super.name);
-		super.sayHello();
+		System.out.print(super.name);	//super.name은 부모클래스의 name을 출력
+		super.sayHello();	//부모 클래스에 있는 sayHello() 메소드를 호출
 	}
 	
 	public static void main(String[] args) {
