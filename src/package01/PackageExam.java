4b008cb650163fd437243022704dd8d89f7bbe9f
yday <yangdayeon2@gmail.com>
2022-04-19 16:00:15 +0900

    java.lang

---
+++ src/package01/PackageExam.java
@@ -0,0 +1,11 @@
+package package01;
+// import java.lang.*;
+// import java.lang.String;
+// java.lang 패키지는 import를 하지 않아도 됨
+public class PackageExam {
+    public static void main(String[] args) {
+        String strData = "I Love You";
+
+        System.out.println("strData = " + strData);
+    }
+}
