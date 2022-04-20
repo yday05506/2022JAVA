cc8ecec237f0aaad9629d0da657079f02dc67a57
yday <yangdayeon2@gmail.com>
2022-04-20 10:55:52 +0900

    valueOf

--- src/package01/ValueOfExam.java
+++ src/package01/ValueOfExam.java
@@ -6,6 +6,7 @@ public class ValueOfExam {
         String strData1 = "A";  // 문자열
         String strData2;
         // ch가 가지고 있는 문자 데이터를 문자열로 변경
+        // valueOf(ch); 메소드는 static으로 정의 되어있어 클래스 이름으로 호출
         strData2 = String.valueOf(ch);
 
         if(strData1.equals(strData2))
