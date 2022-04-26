7b226597fdef1e5e4548cb500e8949bbe42d6334
yday <yangdayeon2@gmail.com>
2022-04-26 16:09:46 +0900

    Math.abs

---
+++ src/package03/AbsExam.java
@@ -0,0 +1,11 @@
+package package03;
+
+public class AbsExam {
+    public static void main(String[] args) {
+        // abs는 absolute(절대값)로, 무조건 양수 출력
+        System.out.println(Math.abs(25));
+        System.out.println(Math.abs(-25));
+        System.out.println(Math.abs(25.5));
+        System.out.println(Math.abs(-25.5));
+    }
+}
