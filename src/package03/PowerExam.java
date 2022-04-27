061a7da64e2994f6ba0dff17a69637ff977a443c
yday <yangdayeon2@gmail.com>
2022-04-27 10:42:51 +0900

    pow()

---
+++ src/package03/PowerExam.java
@@ -0,0 +1,11 @@
+package package03;
+
+public class PowerExam {
+    public static void main(String[] args) {
+        System.out.println(Math.pow(3, 2)); // 3의 2승 == 9.0
+        System.out.println(Math.pow(3.0f, 2.0f));
+        System.out.println(Math.pow(5.0, 2));
+        // 거듭제곱을 구하는 메소드 -5의 3.0승 == -125
+        System.out.println(Math.pow(-5.0, 3.0));
+    }
+}
