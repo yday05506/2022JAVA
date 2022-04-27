6bc4d5bdbc77e2b48a838e5e155292de8eac7b07
yday <yangdayeon2@gmail.com>
2022-04-27 10:45:41 +0900

    sqrt()

---
+++ src/package03/SquareExam.java
@@ -0,0 +1,11 @@
+package package03;
+
+public class SquareExam {
+    public static void main(String[] args) {
+        // sqrt는 square root의 약자
+        System.out.println(Math.sqrt(9));
+        System.out.println(Math.sqrt(9.0f));
+        System.out.println(Math.sqrt(9.0));
+        System.out.println(Math.sqrt(-9.0));
+    }
+}
